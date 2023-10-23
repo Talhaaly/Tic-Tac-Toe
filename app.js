ba84016c6f962408c2b0b84e47fb30ca34afd489
talha <talhaaly103@gmail.com>
2023-10-23 14:58:11 +0500

    Tic-Tac-Toe.Game

--- app.js
+++ app.js
@@ -17,20 +17,22 @@ const changeTurn = () =>{
 const chechWin = () => {
     let boxTexts = document.getElementsByClassName('boxText')
 let wins = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0,1,2, 5,5,0],
+    [3,4,5, 5,15,0],
+    [6,7,8, 5,25,0],
+    [0,3,6, -5,15,90],
+    [1,4,7, 5,15,90],
+    [2,5,8, 15,15,90],
+    [0,4,8, 5,15,45],
+    [2,4,6, 5,15,135]
 ]
 wins.forEach(e => {
     if((boxTexts[e[0]].innerText === boxTexts[e[1]].innerText && (boxTexts[e[1]].innerText === boxTexts[e[2]].innerText)) && (boxTexts[e[0]].innerText !== '') ){
         document.querySelector('.info').innerText = boxTexts[e[0]].innerText + " " + "won"
         isgameover = true;
-        document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px"
+        document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px";
+        document.querySelector('.line').style.width = "25vw";
+        document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
     }
 })
 }
@@ -63,6 +65,7 @@ reset.addEventListener('click', () =>{
     });
     turns = "X"
     isgameover = false;
+    document.querySelector('.line').style.width = "0";
     document.getElementsByClassName('info')[0].innerText = "Turn For" + turns;
     document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0px"
 })
